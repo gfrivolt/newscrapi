6142ea87ff741da522b1b20cd51aab7d6662760d
Gyorgy Frivolt <gyorgy.frivolt@gmail.com>
2010-02-13 13:49:57 +0100

    fixed yml to rb config file

--- rails/init.rb
+++ rails/init.rb
@@ -1,3 +1,3 @@
 require 'content_scrapper'
 
-ContentScrapper.default_config_file = "#{RAILS_ROOT}/config/content_scrapper.yml"
+ContentScrapper.default_config_file = "#{RAILS_ROOT}/config/content_scrapper.rb"
